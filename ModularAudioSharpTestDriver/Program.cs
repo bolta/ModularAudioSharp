717eb7fe0dde0d677ffab661b5aec57579d455d6
bolta <freshness.and.speed.2007@gmail.com>
2017-10-20 00:22:32 +0900

    古い検証コード（コンパイルが通らなくなっていた）を削除

--- ModularAudioSharpTestDriver/Program.cs
+++ ModularAudioSharpTestDriver/Program.cs
@@ -15,7 +15,6 @@ using static ModularAudioSharp.Nodes;
 namespace ModularAudioTestDriver {
 	class Program {
 		static void Main(string[] args) {
-#if true
 			//			VarSample();
 			//SeqSample();
 			//			ParserSample();
@@ -24,75 +23,8 @@ namespace ModularAudioTestDriver {
 
 			//NoteSample();
 			StereoSample();
-#elif true
-			{
-				var tempo = Nodes.Const(120f);
-				tempo.Update();
-				var tick = Sequencer.Tick(tempo, 48);
-				tick.Update();
-				foreach (var t in tick.UseAsStream()) {
-					Console.WriteLine(t);
-					tick.Update();
-				}
-				return;
-			}
-#elif true
-			{
-				var tempo = 120 + 80 * SinOsc(0.125f);
-				var tick = new Tick(tempo, 48);
-				var seq = SequencerExper1.New(tick, 48, 1);
-				var osc = SquareOsc(seq.GetMember(s => s.Freq));
-
-				var master = 0.125f * osc;
-
-				using (var player = ModuleSpace.Play((Node<float>) master)) {
-					//				Thread.Sleep(100 * 1000);
-					for (var i = 0 ; ; ++i) {
-						Console.WriteLine(i);
-						Thread.Sleep(1 * 1000);
-					}
-				}
-			}
-#else
-			{
-				var tempo = 120 + 80 * SinOsc(0.125f);
-				var tick = new Tick(tempo, 48);
-				tick.Name = "tick";
-				var seq = Sequencer_old.New(tick, 48);
-
-				var temper = Temperament.Equal(seq.GetMember(s => s.Tone));
-
-				var osc = SquareOsc(temper);
-
-				var master = 0.125f * osc;
-
-				{
-					for (var i = 0 ; i < 40 ; ++i) {
-						seq.AddInstruction(48 * i + 0, o => new Sequencer_old.Output {
-							Tone = new Tone { Octave = 3, ToneName = ToneName.A, Accidental = 0 },
-						});
-						seq.AddInstruction(48 * i + 12, o => new Sequencer_old.Output {
-							Tone = new Tone { Octave = 4, ToneName = ToneName.E, Accidental = 0 },
-						});
-						seq.AddInstruction(48 * i + 24, o => new Sequencer_old.Output {
-							Tone = new Tone { Octave = 4, ToneName = ToneName.A, Accidental = 0 },
-						});
-						seq.AddInstruction(48 * i + 36, o => new Sequencer_old.Output {
-							Tone = new Tone { Octave = 5, ToneName = ToneName.C, Accidental = 1 },
-						});
-					}
-
-					using (var player = ModuleSpace.Play((Node<float>) master)) {
-						//				Thread.Sleep(100 * 1000);
-						for (var i = 0 ; ; ++i) {
-							Console.WriteLine(i);
-							Thread.Sleep(1 * 1000);
-						}
-					}
-				}
-			}
-#endif
 		}
+
 		private static void VarSample() {
 			var freq = Var(440f);
 			var sin = SinOsc(freq) * 0.125f;
