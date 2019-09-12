07f2dfc34cf533276c478a0d39cab6a0a88869f8
bolta <freshness.and.speed.2007@gmail.com>
2019-09-13 00:13:03 +0900

    #37 パフォーマンス計測用に、音を出さずに最速でサンプルを生成する Output を追加

--- Moddl/Program.cs
+++ Moddl/Program.cs
@@ -17,11 +17,23 @@ namespace Moddl {
 
 			var moddlPath = args[0];
 			var moddl = File.ReadAllText(moddlPath);
+#if true
 			using (var output = new AudioOutput<float>()) {
 				new Player().Play(moddl, output);
 				Console.WriteLine("Press any key to terminate.");
 				Console.ReadKey();
 			}
+#else
+			var timeSecs = 30;
+			var output = new NullOutput<float>(timeSecs * 44100);
+			var startTime = DateTime.Now;
+			new Player().Play(moddl, output);
+			var elapsedTime = DateTime.Now - startTime;
+			Console.WriteLine(elapsedTime);
+			Console.WriteLine(string.Format("Generation efficency: {0}", timeSecs / elapsedTime.TotalSeconds));
+#endif
+			Console.ReadKey();
+
 		}
 	}
 }
---
+++ ModularAudioSharp/Output/NullOutput.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModularAudioSharp.Output {
+	public class NullOutput<T> : Output<T> where T: struct {
+
+		private readonly int samplesToPlay;
+
+		public NullOutput(int samplesToPlay) {
+			this.samplesToPlay = samplesToPlay;
+		}
+
+		public override void Play(IEnumerable<T> signal) {
+			int count = 0;
+			foreach (var sample in signal.Take(this.samplesToPlay)) {
+				++ count;
+				if (count % ModuleSpace.SampleRate == 0) {
+					Console.WriteLine($"Elapsed samples: {count}");
+				}
+			}
+		}
+		//public void Stop();
+	}
+}
